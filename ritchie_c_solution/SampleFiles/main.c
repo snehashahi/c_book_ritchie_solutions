7931f221ff2faf07fef6dba8edd5b09a67e6c404
tuddi <snehashahi900@gmail.com>
2020-02-15 12:55:33 -0800

    Moves sample files to a seperate directory
    Signed-off-by: tuddi <snehashahi900@gmail.com>

---
+++ ritchie_c_solution/SampleFiles/main.c
@@ -0,0 +1,20 @@
+/**
+File         : main.c
+Description  : 
+Author       : sneha
+Date         : 
+Version      : 1.0.0
+*/
+
+#include "stdio.h"
+
+/*****************************************************/
+/*Select the follwing flag to run the particular test*/
+
+/*****************************************************/
+
+int main()
+{
+  return 0;
+}
+
