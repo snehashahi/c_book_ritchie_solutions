318877c29459bce6a8c5eb3dada259eb69b4140b
snehashahi <snehashahi900@gmail.com>
2020-06-03 06:37:42 -0700

    Add farenhiet to celsius table
    Signed-off-by: snehashahi <snehashahi900@gmail.com>

--- ritchie_c_solution/chapter1/Functions/main.c
+++ ritchie_c_solution/chapter1/Functions/main.c
@@ -11,6 +11,7 @@ Version      : 1.0.0
 /*****************************************************/
 /*Select the follwing flag to run the particular test*/
 #define CACLCULATE_POWER    0 // calculate power i
+#define EXERCISE_1_15       0 //Print farenhiet to celsius table
 /*****************************************************/
 #if CACLCULATE_POWER
 int power(int base, int exp);
@@ -25,11 +26,30 @@ int pow = 1;
  return pow;
 }
 #endif
+
+#if EXERCISE_1_15
+void faren_2_celsius_table()
+{
+  int lower = 0;
+  int upper = 300;
+  float celsius = 0;
+  printf("Faren\t\tCelsius\n");
+  for(int i = lower; i< upper ; i+=20)
+  {
+    celsius = ((i - 32) * 5.0f) /9.0f;
+    printf("%3d\t\t%2f\n", i, celsius);
+  }
+}
+#endif
 int main()
 {
 #if CACLCULATE_POWER
   int pow = power (5, 2);
   printf("power is %d\n", pow);
+#endif
+
+#if EXERCISE_1_15
+  faren_2_celsius_table();
 #endif
   return 0;
 }
