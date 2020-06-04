199b74d14a1ea1c8a45838828ac045ad7305ab12
snehashahi <snehashahi900@gmail.com>
2020-06-04 07:10:46 -0700

    Add example1_!7
    Signed-off-by: snehashahi <snehashahi900@gmail.com>

--- ritchie_c_solution/chapter1/Char_Arrays/main.c
+++ ritchie_c_solution/chapter1/Char_Arrays/main.c
@@ -11,18 +11,16 @@ Version      : 1.0.0
 /*****************************************************/
 /*Select the follwing flag to run the particular test*/
 #define COPY_LONGEST_LINE   0 //Finds the getline and copy the maximum line
+#define EXERCISE_1_18       0 //Print all the input line that are greater than 80 character
 /*****************************************************/
-
-#if COPY_LONGEST_LINE
 
 int getlineL(char line[], int max_length);
 void copy(char dest[], char src[]);
 #define MAX_SIZE 1000 //maximumsize of the array
 
-#endif //COPY_LONGEST_LINE
-
 int main()
 {
+#if COPY_LONGEST_LINE
   char line[MAX_SIZE];
   char longestLine[MAX_SIZE];
   int length, max;
@@ -37,7 +35,21 @@ int main()
   }
   if(max > 0)
     printf("length of line =%d line =%s \n", max, longestLine); 
-            
+#endif
+
+#if EXERCISE_1_18
+  char curr_line[MAX_SIZE];
+  
+  int length;
+  length = 0;
+  
+  while((length = getlineL(curr_line, MAX_SIZE)) > 0)
+  {
+    if(length > 8)
+      printf("array = %s length of array = %d",curr_line, length);
+  }
+   
+#endif            
   return 0;
 }
 
