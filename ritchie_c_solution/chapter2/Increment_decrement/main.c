6ab51de3b981309ee7a2ccdd1d4f12f06140ff34
snehashahi <snehashahi900@gmail.com>
2020-09-03 07:48:09 -0700

    Added string cat examples
    Signed-off-by: snehashahi <snehashahi900@gmail.com>

--- ritchie_c_solution/chapter2/Increment_decrement/main.c
+++ ritchie_c_solution/chapter2/Increment_decrement/main.c
@@ -11,7 +11,9 @@ Version      : 1.0.0
 /*****************************************************/
 /*Select the follwing flag to run the particular test*/
 #define EXAMPLES_SQUEEZE     0   //Squeeze the character from array
+#define EXAMPLES_STRCAT      0   //Conctanate 2 strings
 /*****************************************************/
+
 /**
 Remove the character from the array
 @input s[] input array
@@ -29,11 +31,37 @@ void squeeze(char s[], char c)
   s[j] = '\0';
   printf("the string is %s\n", s);
 }
+
+/**
+Concatenate 2nd array at the end of first array
+@input char [] InputArray1
+@input char[] InputArray2
+return none
+*/
+void strcatLocal(char inputArray1[], char inputArray2[])
+{
+  int index1, index2;
+  index1 = index2 = 0;
+  while(inputArray1[index1] != '\0')
+   index1++;
+  while((inputArray1[index1] = inputArray2[index2]) != '\0')
+  {
+    index1++; 
+    index2++;
+  }
+  printf("concatenated str %s  \n", inputArray1);
+}
 int main()
 {
 #if EXAMPLES_SQUEEZE
   char inputArray[6] = "abcdef";
   squeeze(inputArray, 'c');
+#endif
+
+#if EXAMPLES_STRCAT
+  char array1[11] = "abcdef";
+  char array2[10] = "ghijk";
+  strcatLocal(array1, array2);
 #endif
   return 0;
 }
