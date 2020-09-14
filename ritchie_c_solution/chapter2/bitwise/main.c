e80e1f9e0d609da898343b495918e1106463f6d4
snehashahi <snehashahi900@gmail.com>
2020-09-14 07:30:23 -0700

    Added inverted program
    Signed-off-by: snehashahi <snehashahi900@gmail.com>

--- ritchie_c_solution/chapter2/bitwise/main.c
+++ ritchie_c_solution/chapter2/bitwise/main.c
@@ -11,17 +11,37 @@ Version      : 1.0.0
 /*****************************************************/
 /*Select the follwing flag to run the particular test*/
 #define EXERCISE_2_6          0 //Set bits from one position
+#define EXERCISE_2_7          0 //Invert the number
 /*****************************************************/
+/**
+setbits it returns x with n bits  starting at posiion p with n bits
+*/
 int setbits(int x , int p, int n, int y)
 {
   return (x & ~(~(~0 << n) << (p + 1 - n))) | (y & ~(~(~0 << n) << (p + 1- n))) ;
 }
 
+/**
+Invert :Return x with n bits that begins at position p and inverted
+@int x :the number
+@int p :position
+@int n :number of bits
+return inverted number
+*/
+int invert(int x, int p, int n)
+{
+  return x ^ (~(~0 << n) << (p + 1 - n));
+}
+
 int main()
 {
 #if EXERCISE_2_6
-  int num = setbits(10, 1, 2, 5);
-  printf("The number is %d\n", num);
+  int setNum = setbits(10, 1, 2, 5);
+  printf("The number is %d\n", setNum);
+#endif
+#if EXERCISE_2_7
+  int invertedNum = invert(8, 3, 3);
+  printf("The inverted number is %d\n", invertedNum);
 #endif
   return 0;
 }
