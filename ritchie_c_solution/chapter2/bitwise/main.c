ade16fd7a5853c17ba42a7ccc985bd2a49318b4c
snehashahi <snehashahi900@gmail.com>
2020-09-15 07:42:52 -0700

    Add bitwise operator
    Signed-off-by: snehashahi <snehashahi900@gmail.com>

--- ritchie_c_solution/chapter2/bitwise/main.c
+++ ritchie_c_solution/chapter2/bitwise/main.c
@@ -13,6 +13,8 @@ Version      : 1.0.0
 #define EXERCISE_2_6          0 //Set bits from one position
 #define EXERCISE_2_7          0 //Invert the number
 #define EXERCISE_2_8          0 //Right rotation
+#define COUNT_SET_BITS        0 //Count number of setbits 
+#define EXERCISE_2_9          0 //Count number of setbits faster way
 /*****************************************************/
 /**
 setbits it returns x with n bits  starting at posiion p with n bits
@@ -43,6 +45,33 @@ int rightrot(int x, int n)
 {
   return(((x & ~(~0 << n)) << ((sizeof(x) * 8) - n)) | (x >> n));
 }
+/**
+Count number of set bits in a number
+input x
+return number of setbits
+*/
+int countsetbits(int x)
+{
+  int numSetBits;
+  for(numSetBits = 0;x != 0; x >>= 1)
+  {
+     if(x & 1)
+       numSetBits++;
+  }
+  return numSetBits;
+}
+/**
+count number of setbits in faster way
+@input x
+return set bits
+*/
+int fasterSetbits(int x)
+{
+  int numSetBits;
+  for(numSetBits = 0; x != 0; x &= (x-1))
+    numSetBits++;
+  return numSetBits;
+}
 int main()
 {
 #if EXERCISE_2_6
@@ -56,6 +85,14 @@ int main()
 #if EXERCISE_2_8
   int rightrotdNum = rightrot(0xf5f5ff00, 16);
   printf("the right rotaed number %x\n", rightrotdNum);
+#endif
+#if COUNT_SET_BITS
+  int setbits = countsetbits(5);
+  printf("setbits %d\n", setbits);
+#endif
+#if EXERCISE_2_9
+ int numSetbit = fasterSetbits(5);
+ printf("faster set bits %d\n", numSetbit);
 #endif
   return 0;
 }
